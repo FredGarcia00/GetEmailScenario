f99ff24b71d999712c7d173aee52b766b90039de
Fred Garcia <Fred@Garcia.com>
2021-07-26 14:47:56 -0700

    finished popup modal to get users email

--- src/EmailModal/EmailProvider.js
+++ src/EmailModal/EmailProvider.js
@@ -10,19 +10,62 @@ export function useStateContext() {
 export function EMProvider({ children }) {
   const [modalOpen, setModalOpen] = useState(false);
   const openModalAction = () => {
-    Cookies.set("modalOpenBefore", true, { expires: 7 });
+    Cookies.set("modalOpenBefore", true, { expires: 2 });
     setModalOpen(true);
   };
   const closeModalAction = () => {
     setModalOpen(false);
   };
- 
-  const [email, setEmail] = useState('');
+
+  const [email, setEmail] = useState("");
   const handleEmailInput = (e) => {
     setEmail(e.target.value);
   };
- 
+
+  const [showEmailError, setShowEmailError] = useState(false);
+  const checkForEmail = (e) => {
+    function emailIsValid(text) {
+      //regex below
+      return /\S+@\S+\.\S+/.test(text);
+    }
+    if (!emailIsValid(email)) {
+      setShowEmailError(true);
+    }
+    // console.log('checking for email');
+  };
+
+  const removeErrorMessage = (e) => {
+    setShowEmailError(false);
+    console.log("removing error message");
+  };
+
+  const [formCompleted, setFormCompleted] = useState(false);
+  const submittedForm = (e) => {
+    e.preventDefault();
+    if (showEmailError === false && email.length > 5) {
+      setFormCompleted(true);
+      setTimeout(() => {
+        closeModalAction();
+      }, 3000);
+    }
+  };
+
   return (
-    <StateContext.Provider value={{email,handleEmailInput, modalOpen, closeModalAction, openModalAction}}>{children}</StateContext.Provider>
+    <StateContext.Provider
+      value={{
+        email,
+        modalOpen,
+        showEmailError,
+        formCompleted,
+        checkForEmail,
+        removeErrorMessage,
+        submittedForm,
+        handleEmailInput,
+        closeModalAction,
+        openModalAction,
+      }}
+    >
+      {children}
+    </StateContext.Provider>
   );
 }
